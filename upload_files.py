cf7d24db666e64b14816d01431b520a59c8d6469
crtahlin <crt.ahlin@gmail.com>
2023-09-17 21:06:51 +0200

    add termination catch

--- upload_files.py
+++ upload_files.py
@@ -6,6 +6,8 @@ import argparse
 import subprocess
 from datetime import datetime
 import re
+import tempfile
+import shutil
 
 def load_settings(settings_path):
     with open(settings_path, 'r') as f:
@@ -16,8 +18,9 @@ def load_filelist(filelist_path):
         return json.load(f)
 
 def save_filelist(filelist, filelist_path):
-    with open(filelist_path, 'w') as f:
-        json.dump(filelist, f, indent=4)
+    with tempfile.NamedTemporaryFile('w', dir=os.path.dirname(filelist_path), delete=False) as tempf:
+        json.dump(filelist, tempf, indent=4)
+    shutil.move(tempf.name, filelist_path)
 
 def upload_file(file_info, settings):
     cmd = ['swarm-cli', 'upload', file_info['full_path'], '--quiet']
@@ -56,38 +59,43 @@ def upload_file(file_info, settings):
         return {"timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "error": str(e)}
 
 if __name__ == '__main__':
-    successful_count = 0
-    unsuccessful_count = 0
-    total_data_uploaded = 0  # In bytes
+    try:
+        successful_count = 0
+        unsuccessful_count = 0
+        total_data_uploaded = 0  # In bytes
+        
+        parser = argparse.ArgumentParser(description='Upload files.')
+        parser.add_argument('-S', '--settings', help='Path to settings YAML file', default='settings.yaml')
+        args = parser.parse_args()
+
+        settings = load_settings(args.settings)
+        file_list = load_filelist(settings['file_info_path'])
     
-    parser = argparse.ArgumentParser(description='Upload files.')
-    parser.add_argument('-S', '--settings', help='Path to settings YAML file', default='settings.yaml')
-    args = parser.parse_args()
+        upload_filter = settings.get('upload_filter', 'all')  # Default to 'all' if not specified
+    
+        for file_info in file_list:
+            # Skip this file if we are uploading only 'pending' files and this file has a swarmHash
+            if upload_filter == 'pending' and 'swarmHash' in file_info:
+                continue
 
-    settings = load_settings(args.settings)
-    file_list = load_filelist(settings['file_info_path'])
-
-    upload_filter = settings.get('upload_filter', 'all')  # Default to 'all' if not specified
-
-    for file_info in file_list:
-        # Skip this file if we are uploading only 'pending' files and this file has a swarmHash
-        if upload_filter == 'pending' and 'swarmHash' in file_info:
-            continue
-
-        upload_attempt = upload_file(file_info, settings)
+            upload_attempt = upload_file(file_info, settings)
         
-        if "error" not in upload_attempt:
-            successful_count += 1
-            total_data_uploaded += file_info['size']
-            file_info.setdefault('upload_attempts', []).append(upload_attempt)  # Set default for 'upload_attempts' if not exists
-        else:
-            unsuccessful_count += 1
-            file_info.setdefault('upload_attempts', []).append(upload_attempt)  # Set default for 'upload_attempts' if not exists
-        
+            if "error" not in upload_attempt:
+                successful_count += 1
+                total_data_uploaded += file_info['size']
+                file_info.setdefault('upload_attempts', []).append(upload_attempt)  # Set default for 'upload_attempts' if not exists
+            else:
+                unsuccessful_count += 1
+                file_info.setdefault('upload_attempts', []).append(upload_attempt)  # Set default for 'upload_attempts' if not exists
+            
+            save_filelist(file_list, settings['file_info_path'])
+    
+        total_data_uploaded_MB = total_data_uploaded / (1024 * 1024)
+        print(f"\nSuccessfully uploaded {successful_count} files.")
+        print(f"Failed to upload {unsuccessful_count} files.")
+        print(f"Total data uploaded: {total_data_uploaded_MB:.2f} MBytes")
+    except KeyboardInterrupt:
+        print("CTRL-C detected. Attempting to save JSON file before exiting.")
         save_filelist(file_list, settings['file_info_path'])
-
-    total_data_uploaded_MB = total_data_uploaded / (1024 * 1024)
-    print(f"\nSuccessfully uploaded {successful_count} files.")
-    print(f"Failed to upload {unsuccessful_count} files.")
-    print(f"Total data uploaded: {total_data_uploaded_MB:.2f} MBytes")
+        print("JSON file saved. Exiting now.")
 
