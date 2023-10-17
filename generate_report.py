bd9de270b812f73b58678edf305c8951d4f0729b
crtahlin <crt.ahlin@gmail.com>
2023-10-17 17:02:14 +0200

    fix empty timestamp exception

--- generate_report.py
+++ generate_report.py
@@ -2,6 +2,8 @@ import json
 import yaml
 import csv
 import argparse
+from datetime import datetime
+
 
 def load_settings(settings_path):
     with open(settings_path, 'r') as f:
@@ -11,27 +13,23 @@ def load_filelist(filelist_path):
     with open(filelist_path, 'r') as f:
         return json.load(f)
 
+
 def calculate_speed_MBps(timestamp_start, timestamp_end, file_size_bytes):
-    from datetime import datetime
+    if not timestamp_start or not timestamp_end:
+        return ''
     FMT = '%Y-%m-%d %H:%M:%S'
     tdelta = datetime.strptime(timestamp_end, FMT) - datetime.strptime(timestamp_start, FMT)
     duration_seconds = tdelta.total_seconds()
+    if duration_seconds == 0:
+        return "Infinite"
     file_size_MB = file_size_bytes / (1024 * 1024)
-    
-    if duration_seconds > 0:
-        speed_MBps = file_size_MB / duration_seconds
-        return round(speed_MBps, 2)
-    else:
-        return None  # To handle division by zero if timestamps are the same
+    return round(file_size_MB / duration_seconds, 2)
 
 def generate_csv_report(file_list, output_path):
     with open(output_path, 'w', newline='') as csvfile:
-        fieldnames = [
-            'filename', 'size', 'swarmHash', 'first uploaded', 'last uploaded',
-            'last successful download', 'number of times successfully downloaded',
-            'last successful download speed (MB/s)', 'last successful upload speed (MB/s)'
-        ]
-        
+        fieldnames = ['filename', 'size', 'swarmHash', 'first uploaded', 'last uploaded', 
+                      'last successful download', 'number of times successfully downloaded', 
+                      'last successful download speed (MB/s)', 'last successful upload speed (MB/s)']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         
@@ -39,25 +37,20 @@ def generate_csv_report(file_list, output_path):
             successful_uploads = [attempt for attempt in file_info.get('upload_attempts', []) if 'error' not in attempt]
             successful_downloads = [attempt for attempt in file_info.get('download_attempts', []) if attempt.get('sha256_comparison') == 'Successful']
 
-            # Get the speed of the last successful upload if available
-            last_successful_upload = successful_uploads[-1] if successful_uploads else None
-            last_upload_speed = None
-            if last_successful_upload:
-                last_upload_speed = calculate_speed_MBps(
-                    last_successful_upload.get('timestamp_start', ''),
-                    last_successful_upload.get('timestamp_end', ''),
-                    file_info.get('size', 0)
-                )
-            
-            # Get the speed of the last successful download if available
-            last_successful_download = successful_downloads[-1] if successful_downloads else None
-            last_download_speed = None
-            if last_successful_download:
-                last_download_speed = calculate_speed_MBps(
-                    last_successful_download.get('timestamp_start', ''),
-                    last_successful_download.get('timestamp_end', ''),
-                    file_info.get('size', 0)
-                )
+            last_successful_upload = max(successful_uploads, key=lambda x: x.get('timestamp_end', ''), default=None)
+            last_successful_download = max(successful_downloads, key=lambda x: x.get('timestamp_end', ''), default=None)
+
+            last_upload_speed = calculate_speed_MBps(
+                last_successful_upload.get('timestamp_start', '') if last_successful_upload else '',
+                last_successful_upload.get('timestamp_end', '') if last_successful_upload else '',
+                file_info.get('size', 0)
+            )
+
+            last_download_speed = calculate_speed_MBps(
+                last_successful_download.get('timestamp_start', '') if last_successful_download else '',
+                last_successful_download.get('timestamp_end', '') if last_successful_download else '',
+                file_info.get('size', 0)
+            )
 
             writer.writerow({
                 'filename': file_info.get('filename', ''),
